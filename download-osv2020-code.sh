149f4f025bad5eb81d9ef3f4af69e33f3e943348
Mark Janssen <20283+praseodym@users.noreply.github.com>
2026-04-17 10:13:13 +0200

    Update OSV2020-U download URL

--- download-osv2020-code.sh
+++ download-osv2020-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-wget https://www.kiesraad.nl/binaries/kiesraad/documenten/publicaties/2026/02/02/broncode-osv2020-u---gr26/broncode+osv2020-u-versie-1.12.7.2.zip
+wget https://www.kiesraad.nl/site/binaries/site-content/collections/documents/2026/02/02/broncode-osv2020-u---gr26/broncode+osv2020-u-versie-1.12.7.2.zip
 unzip broncode+osv2020-u-versie-1.12.7.2.zip "osv2020-u-versie-1.12.7.2/*"
 rm broncode+osv2020-u-versie-1.12.7.2.zip
